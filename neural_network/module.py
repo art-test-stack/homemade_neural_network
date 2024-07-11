ee8ada6dc6db22b117195660e21f2035015da819
Arthur Testard <testardarthur@gmail.com>
2024-07-11 18:33:39 +0200

    new arch basis

---
+++ neural_network/module.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class Module():
+    def __init__(self, no_grad: bool = False, bias: bool = True) -> None:
+        self.no_grad = no_grad
+        self.bias = bias
+        self.last_x = None
+    
+    def __call__(self, x):
+        self.last_x = x if not self.no_grad else self.last_x
+        return self.forward(x)
+    
+    def forward(self, x):
+        NotImplementedError("Implement self forward method")
+
+    def grad(self, x):
+        NotImplementedError("Implement self grad method")
+
+    def backward(self, J):
+        g = J * self.grad(self.last_x)
+        if not self.no_grad:
+            self.dw = (self.last_x).T.dot(g) / self.last_x.shape[0] + self.wreg * self.w_reg(self.W)
+            if self.bias:
+                self.db = np.mean(g, axis = 0) + self.wreg * self.w_reg(self.b)
+        return g
---
+++ neural_network/optimizer.py
@@ -0,0 +1,8 @@
+
+
+class SGD:
+    def __init__(self, parameters) -> None:
+        self.parameters = parameters
+
+    def update_parameters(self) -> None:
+        None
